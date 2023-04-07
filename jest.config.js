f00119f395ab7850fc62e38587674083adde8221
majapytlasinska <maja.pytlasinska@oke.pl>
2023-04-07 09:18:30 +0200

    tests for pet part

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  moduleDirectories: [
+    'node_modules'
+  ],
+  transform: {
+    "\\.tsx?$": "ts-jest",
+    "\\.jsx?$": "babel-jest", // if you have jsx tests too
+  },
+  globals: {
+    "ts-jest": {
+      "tsConfig": '<rootDir>/tsconfig.json'
+    }
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\](?!lodash-es/).+\\.js$"
+  ],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
